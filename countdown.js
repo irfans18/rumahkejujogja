39ec70357d7cfb92adc0a662aebcd3396708f8fa
irfans18 <58197653+irfans18@users.noreply.github.com>
2021-12-10 18:13:37 +0700

    configure promo message

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 const countdown = () =>{
    
    const gap = initDate();
-   console.log(gap);
+   // console.log(gap);
 
    const second = 1000;
    const minute = second * 60;
@@ -17,8 +17,21 @@ const countdown = () =>{
    document.getElementById("day").textContent = format(viewDay);
    document.getElementById('hour').textContent = format(viewHour);
    document.getElementById("minute").textContent = format(viewMinute);
-   document.getElementById("second").textContent = format(viewSecond);
+   document.getElementById("second").textContent = format(viewSecond); 
+   // document.getElementById("firstLineText").textContent = "Menuju";
 
+
+}
+
+const promoMessage = (value) => {
+
+   if(value == false) {
+      document.getElementById("firstLineText").textContent = "Menuju";
+      document.getElementById("secondLineText").textContent = "PROMO 12.12";
+   } else {
+      document.getElementById("firstLineText").textContent = "PROMO 12.12";
+      document.getElementById("secondLineText").textContent = "Diskon 12%";
+   }
 }
 
 const initDate = () =>{
@@ -27,8 +40,10 @@ const initDate = () =>{
    const now = new Date().getTime();
 
    if (now < startDate) {
+      promoMessage(false);
       return startDate - now;
    } else {
+      promoMessage(true);
       return endDate - now;
    }
 }
